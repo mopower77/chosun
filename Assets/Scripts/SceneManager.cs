bc783a5dfcaad842be4689620aae40499866531f
PC053\Administrator <mopower77@naver.com>
2017-10-10 15:12:19 +0900

    -리소스 추가

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : SingletonM<SceneManager>
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    public void SceneLoad(string strsceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(strsceneName);
+    }
+
+    private IEnumerator SceneLoadCoroutine(string strsceneName)
+    {
+        yield return 0;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(strsceneName);
+
+    }
+
+
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonM<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instacne
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType(typeof(T)) as T;
+                if(instance == null)
+                {
+                    // 생성
+                    GameObject obj = new GameObject();
+                    obj.AddComponent<T>();
+                    instance = obj.GetComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
